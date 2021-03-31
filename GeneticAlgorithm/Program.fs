1ad1bceaefef50645b976868623a2f25e1e685d3
azurehutchings <azurehutchings@gmail.com>
2021-03-31 15:31:23 +1000

    Completed reverseMutateAt

--- GeneticAlgorithm/Program.fs
+++ GeneticAlgorithm/Program.fs
@@ -94,7 +94,16 @@ let cross (parent1: Individual) (parent2: Individual) : Rand<Individual> =
 // For example reverseMutateAt [0,3,5,4,2,1,6] 2 4 = [0,3,2,4,5,1,6]
 let reverseMutateAt (genes: Individual) (firstIndex: int) (secondIndex: int): Individual =
     // TODO: add correct implementation here 
-    raise (System.NotImplementedException "reverseMutateAt")
+    let firstPart = genes.[..(firstIndex-1)]
+    let secondPart = 
+        genes.[firstIndex..secondIndex]
+        |> Array.rev
+    let thirdPart = genes.[(secondIndex+1)..]
+    firstPart
+    |> Array.append secondPart
+    |> Array.append thirdPart
+
+    //raise (System.NotImplementedException "reverseMutateAt")
 
 // Perform a reverse mutation based on two randomly chosen split points
 // (such that 0 <= firstIndex < secondIndex < genes.length)
